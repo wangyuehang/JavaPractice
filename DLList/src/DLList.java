76400a54d4daa502ba2779a42caca4d4649492e9
Yuehang <wyh98326@gmail.com>
2024-05-17 09:28:52 +0800

    Need Futher implementation

--- DLList/src/DLList.java
+++ DLList/src/DLList.java
@@ -1,4 +1,14 @@
 public class DLList {
+
+    private class intNode{
+        int item;
+        intNode next;
+    }
+
+
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
