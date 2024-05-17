d088ffddd67e2fec609294251e7404a5529a04cb
Yuehang <wyh98326@gmail.com>
2024-05-17 09:54:16 +0800

    #

--- SLList/src/SLList.java
+++ SLList/src/SLList.java
@@ -19,18 +19,43 @@ public class SLList {
 
     /** Add an element to the beginning*/
     public void insertFirst(int i){
-        head = new Node(i, head);
         size ++;
+        head = new Node(i, head);
     }
+
+    public int getFirst(){
+        return head.item;
+    }
+
 
     public void insertLast(int i){
-        
+        size++;
+        while(head.next != null){
+            head = head.next;
+        }
+        head.next = new Node(i, null);
     }
 
+    public int getLast(){
+        while(head.next != null){
+            head = head.next;
+        }
+        return head.item;
+    }
+
+    public int size(){
+        return size;
+    }
 
 
     public static void main(String[] args) {
         SLList myList = new SLList (5);
         myList.insertFirst(3);
+        System.out.println("The first element in this list is: " + myList.getFirst());
+
+        myList.insertLast(7);
+        System.out.println("The last element in this list is: " + myList.getLast());
+
+        System.out.println("The size of this list is: " + myList.size());
     }
 }
